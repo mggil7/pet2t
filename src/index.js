dce6558db0fd803466c0958d52335c7326ee8c84
mggil7 <miguel.gustavo.gil@gmail.com>
2022-02-03 16:04:55 -0300

    v1

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import config from './aws-exports';
 import Amplify from 'aws-amplify';
 import {AmplifyProvider} from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 
 Amplify.configure(config)
